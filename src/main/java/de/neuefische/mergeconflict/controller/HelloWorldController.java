788b09d9442569bdccdc18c34393c066f14bfa27
Matthias Hiemer <matthias.hiemer@neuefische.de>
2022-11-03 10:28:23 +0100

    Move HelloWorld Controller to controller package

--- src/main/java/de/neuefische/mergeconflict/controller/HelloWorldController.java
+++ src/main/java/de/neuefische/mergeconflict/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package de.neuefische.mergeconflict;
+package de.neuefische.mergeconflict.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
